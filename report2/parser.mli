74398a46894d2d814132b5b56bffe4a9bc250ee2
qinghai <qinghai1027@gmail.com>
2025-08-15 23:06:01 +0900

    Implement parser and semantic analysis for new features
    - Added support for MOD and POWER operators in parser.mly and parser.mli.
    - Updated AST representation to include new token types for MOD and POWER.
    - Enhanced semantic analysis to check types for MOD and POWER operations.
    - Introduced error handling for new operators in semantic checks.
    - Updated print_ast.ml to handle new AST nodes for MOD and POWER.
    - Added example programs (prob4, prob6) demonstrating the use of MOD and POWER.
    - Enhanced the symbol table to accommodate new types and declarations.
    - Improved error reporting for syntax and semantic errors.

---
+++ report2/parser.mli
@@ -0,0 +1,51 @@
+type token =
+  | NUM of (
+# 9 "parser.mly"
+        int
+# 6 "parser.mli"
+)
+  | STR of (
+# 10 "parser.mly"
+        string
+# 11 "parser.mli"
+)
+  | ID of (
+# 10 "parser.mly"
+        string
+# 16 "parser.mli"
+)
+  | INT
+  | IF
+  | WHILE
+  | SPRINT
+  | IPRINT
+  | SCAN
+  | EQ
+  | NEQ
+  | GT
+  | LT
+  | GE
+  | LE
+  | ELSE
+  | RETURN
+  | NEW
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIV
+  | LB
+  | RB
+  | LS
+  | RS
+  | LP
+  | RP
+  | ASSIGN
+  | SEMI
+  | COMMA
+  | TYPE
+  | VOID
+  | MOD
+  | POWER
+
+val prog :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.stmt
